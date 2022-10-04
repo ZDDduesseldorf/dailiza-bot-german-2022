770006a1ba2e28317c84d87deea63903be05d0f0
florian-huber <florian.huber@hs-duesseldorf.de>
2022-10-04 17:26:21 +0200

    initial chatbot commit (based on 2nd semester exercise)

---
+++ README.md
@@ -0,0 +1,10 @@
+dailiza-bot
+======
+
+Very simple rule-based chatbot, Eliza-style, speaking German.
+
+Quickstart Guide
+----------------
+
+TODO - fill this in later
+
---
+++ dailiza-bot/dailiza_bot.py
@@ -0,0 +1,31 @@
+import random
+import re
+from reflection import reflect
+from text_patterns import psychobabble
+
+
+def dailiza_answer(user_input):
+    """Diese Funktion generiert die Antwort des DAILIZA-Bot.
+    """
+    user_input = user_input.strip(",.?!")
+
+    # Test input string for all known text patter in pychobabble
+    for pattern, responses in psychobabble:
+        match = re.search(pattern, str(user_input))
+        if match:
+            rspns = random.choice(responses)
+            return rspns.format(*[reflect(g) for g in match.groups()])    
+
+
+def run_dailiza_bot():
+    """Diese Funktion startet den DAILIZA-Bot.
+    """
+    print("Hey Hallo! Ich bin Dailiza. Womit kann ich dir helfen?")
+    user_input = ""
+    while "exit" not in user_input:
+        user_input = input(">> ")
+        print(dailiza_answer(user_input))
+
+
+if __name__ == '__main__':
+    run_dailiza_bot()
