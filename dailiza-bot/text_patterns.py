7d289aece9440fa222f313ff20bd25725c894bae
Liza Kaladjian <lizakalaijiann@gmail.com>
2022-10-06 11:53:49 +0200

    3te Pattern

--- dailiza-bot/text_patterns.py
+++ dailiza-bot/text_patterns.py
@@ -13,4 +13,9 @@ psychobabble = [
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
 
+    [r"[Ii]ch kann (.*) nicht",
+    ["Wie kommst du darauf, dass du {0} nicht kannst",
+    "Vielleicht wirst du es schaffen, wenn du {0} versuchst",
+    "{0} zu können ist meiner Meinung nach nicht so wichtig"]],
+
 ]
