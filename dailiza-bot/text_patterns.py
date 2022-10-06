281dcf8288ba9f04c10af0559e2e75224f47af59
syahidhusein <muhammadsyahid264@gmail.com>
2022-10-06 15:03:29 +0200

    added answers to why + included lowercase

--- dailiza-bot/text_patterns.py
+++ dailiza-bot/text_patterns.py
@@ -13,7 +13,7 @@ psychobabble = [
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
 
-    [r"Warum?",
+    [r"Wwarum?",
     ["Du sagst mir",
     "Keine Ahnung. Ich bin nur ein Rechner.",
     "Das wollte ich auch gleich fragen"]],
