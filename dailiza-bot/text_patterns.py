f1d92d7f0d453658df08d9cb2bc7b4a48d941dc0
Adham Arif <adham.arif@gmail.com>
2022-10-07 19:25:16 +0200

    new list of psychobabble added

--- dailiza-bot/text_patterns.py
+++ dailiza-bot/text_patterns.py
@@ -13,4 +13,48 @@ psychobabble = [
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
 
+    [r"[Ii]ch kann nicht (.*)",
+     ["Warum kannst du nicht {0}?",
+      "Vielleicht musst du nicht {0} zu konnen",
+      "Das kann ich auch nicht"]],
+
+    [r"[Bb]ist du (.*)",
+     ["Vielleicht, weiß ich auch nicht.",
+      "Was denkst du?",
+      "Sicher, wenn du mich das nennen will"]],
+
+    [r"[Ww]as denkst du daran?",
+     ["ich finde es faszinierend und möchte es verstehen.",
+      "Warum interessierst du dich für die Meinung eines Roboters?",
+      "Ich kann mir aus irgendeinem Grund keine Meinung dazu bilden."]],
+
+    [r"[Ww]ie geht(.*)",
+     ["Mir geht's gut",
+      "ein Software-Update entfernt von gut",
+      "Ich verbringe mein ganzes Leben fürs Reden mit Menschen. Was glauben sie, wie es mir geht?"]],
+
+    [r"[Ww]ie alt bist du?",
+     ["Rat mal",
+      "Weder jung noch alt",
+      "Age is just a number, my friend."]],
+
+    [r"[Ww]as ist dein Name?|[Ww]ie hei[sß]s*t du",
+     ["Elisa.",
+      "Du kannst mich Lisa nennen.",
+      "I'm Elisa. But you can call me your friend."]],
+
+    [r"[Ww]as ist dein Lieblings(.*)",
+     ["Ich habe bisher nichts erleben. Also weiß ich selbst nicht.",
+      "Ich habe keine Gefühle, also weiß ich nicht was mein Lieblings{0} ist ",
+      "Ich glaube nicht, dass irgendein Roboter eine Antwort darauf hat."]],
+
+    [r"exit",
+     ["Bye!",
+      "Auf wiedersehen!",
+      "Tchuss"]],
+
+    [r"[^a-zA-Z_üöäß ]",
+     ["???",
+      "Umm...Was für eine Frage ist das?",
+      "Bist du okay???"]],
 ]
