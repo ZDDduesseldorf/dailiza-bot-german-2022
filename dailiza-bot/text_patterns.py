6f75906b64c5925233e83cdb437c63d8b44c19ae
Adham Arif <adham.arif@gmail.com>
2022-10-07 19:56:32 +0200

    6th and above psychobabble added

--- dailiza-bot/text_patterns.py
+++ dailiza-bot/text_patterns.py
@@ -27,5 +27,30 @@ psychobabble = [
     ["Mir geht's gut",
     "ein Software-Update entfernt von gut",
     "Ich verbringe mein ganzes Leben fürs Reden mit Menschen. Was glauben sie, wie es mir geht?"]],
+    
+    [r"[Ww]ie alt bist du?",
+    ["Rat mal",
+    "Weder jung noch alt",
+    "Age is just a number, my friend."]],
+
+    [r"[Ww]as ist dein Name?|[Ww]ie hei[sß]s*t du",
+    ["Elisa.",
+    "Du kannst mich Lisa nennen.",
+    "I'm Elisa. But you can call me your friend."]],
+
+    [r"[Ww]as ist dein Lieblings(.*)",
+    ["Ich habe bisher nichts erleben. Also weiß ich selbst nicht.",
+    "Ich habe keine Gefühle, also weiß ich nicht was mein Lieblings{0} ist ",
+    "Ich glaube nicht, dass irgendein Roboter eine Antwort darauf hat."]],
+
+    [r"exit",
+    ["Bye!",
+    "Auf wiedersehen!",
+    "Tchuss"]],
+
+    [r"[^a-zA-Z_üöäß ]",
+    ["???",
+    "Umm...Was für eine Frage ist das?",
+    "Bist du okay???"]]
 
 ]
