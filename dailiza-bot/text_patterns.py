5b6b4a81deae97deaa21230ae069b268b4fef1ad
l4nz8 <vin.bohm456@gmail.com>
2022-10-11 23:18:16 +0200

    added first psychobabble

--- dailiza-bot/text_patterns.py
+++ dailiza-bot/text_patterns.py
@@ -8,9 +8,13 @@ psychobabble = [
     "Sehr gut, danke. Und wie läuft's bei dir?",
     "Ich kann nicht klagen. Was ist mit dir?"]],
 
-    [r"Ich brauche (.*)",
+    [r"[Ii]ch brauche (.*)",
     ["Warum brauchst du {0}?",
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
 
+    [r"(?<=mir\s)*[\w\s]*gut", 
+    ["Das freut mich, hast du noch irgendeine andere Fragen?",
+    "Kann ich dir noch mit irgendetwas anderem behilflich sein?",
+    "OK cool, brauchst du noch irgendetwas?"]],
 ]
