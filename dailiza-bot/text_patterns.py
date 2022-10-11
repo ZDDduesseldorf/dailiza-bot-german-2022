79c56362044a14403e4b90d59b189a5aab9ac282
Julimaeder <maeder.julian@gmail.com>
2022-10-11 22:47:13 +0200

    3. Pattern hinzugefügt

--- dailiza-bot/text_patterns.py
+++ dailiza-bot/text_patterns.py
@@ -13,4 +13,9 @@ psychobabble = [
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
 
+    [r"Ich wuensche mir (.*)",
+    ["Ist {0} dein innigster Wunsch?",
+    "Wie glücklich würde dich {0} machen?",
+    "Denkst du oft an {0}?"
+    ]]
 ]
