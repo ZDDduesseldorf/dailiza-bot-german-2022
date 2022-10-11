50c15d557cf7a6a93e7362c97137af84c247718e
Justin Bergmann <bergmannjustin03@gmail.com>
2022-10-11 21:38:59 +0200

    hier kurz beschreiben

--- dailiza-bot/text_patterns.py
+++ dailiza-bot/text_patterns.py
@@ -1,3 +1,4 @@
+
 """
 Here we collect the chatbot text patterns.
 """
@@ -13,4 +14,37 @@ psychobabble = [
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
 
-]
+   
+    [r" Wann kommt das neue Cyberpunk?",
+    ["es kommt erst die nächsten Jahre raus",
+     "Okey freust du dich schon?",
+     "Ja klar freue ich mich"]],
+    
+    
+    [r"Ich will mir heute das neue Album von Cro anhören",
+    ["Warum möchtest du dir das Album anhören?",
+    "Weil ich den Künstler sehr gut finde",
+    "Ja dann hör du dir dann später das Album mal an"]],
+
+
+    [r"Wann kommt eine neue Kollektion von LFDY?",
+    ["Nächsten Sommer die Winterkollektion"],
+    "Wie wird die Kollektion aussehen?",
+    "Es werden Winterjacken, sowie Hoodies rauskommen"]],
+ 
+    [r"Ich will heute Andor schauen",
+    ["Wieso willst du dir genau Andor anschauen, wenn es auch andere Serien gibt?",
+    "Ich finde es cool, aber was schlägst du denn vor?",
+    "Ich finde z.B Animes sehr gut."]],
+   
+    ]
+    
+     
+    
+    
+    
+    
+    
+
+
+
