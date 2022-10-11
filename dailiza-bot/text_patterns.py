819230d520b32523b922b52c287bba0c642e707c
EasyTony1734 <zhevnov.anton.00@gmail.com>
2022-10-11 19:03:04 +0200

    hier kurz beschreiben

--- dailiza-bot/text_patterns.py
+++ dailiza-bot/text_patterns.py
@@ -12,5 +12,25 @@ psychobabble = [
     ["Warum brauchst du {0}?",
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
+    
+    [r"Wie viel PS hat Audi RS5?",
+    ["450PS ohne Chip Tuning. Warum fragst du?",
+    "Ich kann mich nicht zwischen Audi RS5 und BMW M8 entscheiden.",
+    "Ich rate dir RS5 zu holen, weil bei BMW immer Motorleuchte leutet xD"]],
+    
+    [r"Ich will gerne heute Fantasie schauen auf Netflix",
+    ["Willst du was neues sehen oder auch altes?",
+    "Kannst du vorschlag für beides machen?",
+    "Klar, Fantastic Four (2015) oder Thor: Love and Thunder (2022)"]],
+    
+    [r"Wann kommt neue Call of Duty raus?",
+    ["Diese Jahr noch Call of Duty Modern Warfere 2 am 05.11.2022",
+    "Cool, gibt es Beta?",
+    "Ja, kannst audf Steam oder Epic Games finden."]],
+    
+    [r"Wann ist Weekly Update in Destiny 2?",
+    ["Jeden Dienstag um 19:00 Deutsche Zeit, ab und zu gibt es wartungsarbeiten ca. bis 20:00",
+    "Was wird an Weekly Update gemacht?",
+    "Alle Bountezuge sowie auch Content Missionen bekommen neue Loot."]],
 
 ]
