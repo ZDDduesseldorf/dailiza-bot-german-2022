a0378f297364066423c7fde84f167afa5f2daef5
mriam-agrawala <m.agrawala@gmx.de>
2022-10-12 10:14:45 +0200

    Soll ich ... lesen?

--- dailiza-bot/text_patterns.py
+++ dailiza-bot/text_patterns.py
@@ -22,4 +22,9 @@ psychobabble = [
     ["Warst du schon mal in {0}?",
     "Würde es dir in {0} gefallen?",
     "Ist {0} sicher?"]],
+    
+    [r"Soll ich (.*) lesen?",
+    ["Würde es dich weiterbringen, {0} zu lesen?",
+    "Interessiert dich {0}?",
+    "Hast du {0} nicht schon gelesen?"]],
 ]
