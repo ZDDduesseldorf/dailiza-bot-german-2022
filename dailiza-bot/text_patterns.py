705112a0120cddd9d6dbbb71ee190830733a4511
Kenneth <nelsonkenneth32@gmail.com>
2022-10-22 20:09:53 +0200

    edited my text_pattern

--- dailiza-bot/text_patterns.py
+++ dailiza-bot/text_patterns.py
@@ -3,36 +3,36 @@ Here we collect the chatbot text patterns.
 """
 
 psychobabble = [
-    [r"geht.{0,5}s.{0,5}dir",
+    [r"[Gg]eht.{0,5}s.{0,5}dir",
     ["Danke. Mir geht es gut und dir?",
     "Sehr gut, danke. Und wie läuft's bei dir?",
     "Ich kann nicht klagen. Was ist mit dir?"]],
 
-    [r"Ich brauche (.*)",
+    [r"[Ii]ch brauche (.*)",
     ["Warum brauchst du {0}?",
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
     
-    [r"Ich würde gern (.*)",
+    [r"[Ii]ch [Ww]ürde gern (.*)",
     ["Warum willst du {0}?",
      "Wird {0} dir denn wirklich helfen?",
      "lonht {0} sich das?"]],
     
-    [r"wie läuft.{0,5}s.{0,5}bei dir?",
+    [r"[Ww]ie [Ll]äuft.{0,5}s.{0,5}bei dir?",
     ["Gut Danke. Und bei dir?",
      "super. Sonst was kann ich für dich tun?"]],
     
-    [r"Ich muss (.*) wissen",
+    [r"[Ii]ch [Mm]uss (.*) [Ww]issen",
     ["{0} ist nicht wichtig für dich, oder?",
      "Bist du sicher, dass du {0} brauchst?",
      "Würde {0} dir denn wirklich helfen?"]],
     
-    [r"Wer ist der beste Fußballspieler der Welt?",
+    [r"[Ww]er ist der beste [Ff]ußballspieler der Welt?",
     ["Der beste Fußballspieler ist Cristiano Ronaldo",
      "Oder Messi",
-     "Sonst vielleicht Benzema"]]
+     "Sonst vielleicht Benzema"]],
     
-    [r"wie hei[sß]s*t du?",
+    [r"[Ww]ie [Hh]ei[sß]s*t du?",
     ["mein Name ist Dailiza",
      "Ich heiße Dailiza und ich bin ein Bot",
      "Ich bin Dailiza"]]
