cba0dec9d4bb27eb64a216668b91e81ff69fe4a2
Anton Zhevnov <zhevnov.anton.00@gmail.com>
2023-01-09 12:27:11 +0300

    I add 4 more Chat request with answers for films, MMo-gaames, Update in game Destiny 2 and about Japan

--- dailiza-bot/text_patterns.py
+++ dailiza-bot/text_patterns.py
@@ -13,24 +13,25 @@ psychobabble = [
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
     
-    [r"Wie viel PS hat Audi RS5?",
-    ["450PS ohne Chip Tuning. Warum fragst du?",
-    "Ich kann mich nicht zwischen Audi RS5 und BMW M8 entscheiden.",
-    "Ich rate dir RS5 zu holen, weil bei BMW immer Motorleuchte leutet xD"]],
-    
     [r"Ich will gerne heute Fantasie schauen auf Netflix",
     ["Willst du was neues sehen oder auch altes?",
-    "Kannst du vorschlag für beides machen?",
-    "Klar, Fantastic Four (2015) oder Thor: Love and Thunder (2022)"]],
+    "Ich schaue keine Filme",
+    "Fantastic Four (2015) oder Thor: Love and Thunder (2022)"]],
     
-    [r"Wann kommt neue Call of Duty raus?",
-    ["Diese Jahr noch Call of Duty Modern Warfere 2 am 05.11.2022",
-    "Cool, gibt es Beta?",
-    "Ja, kannst audf Steam oder Epic Games finden."]],
+    [r"Ich will gerne MMO Game ausprobieren",
+    ["Willst du was neues spielen oder auch altes?",
+    "Du solltest lieber lernen!",
+    "World of Warcraft 2004 oder Destiny 2 2017"]],
+    
     
     [r"Wann ist Weekly Update in Destiny 2?",
-    ["Jeden Dienstag um 19:00 Deutsche Zeit, ab und zu gibt es wartungsarbeiten ca. bis 20:00",
-    "Was wird an Weekly Update gemacht?",
-    "Alle Bountezuge sowie auch Content Missionen bekommen neue Loot."]],
+    ["Jeden Dienstag um 19:00 Deutsche Zeit",
+    "Was ist Destiny 2?",
+    "Ich bin kein Google, google doch einfach selbst!"]],
+    
+    [r"Was kannst du mir über Japan erzählen?",
+    ["Japan ist sehr schön!",
+    "Hauptstadt von Japan ist Tokyo!",
+    "Da leben viele Amine Fans und produzieren selber Hunderte."]],
 
 ]
